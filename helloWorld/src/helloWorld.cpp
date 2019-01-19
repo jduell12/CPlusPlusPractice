8dbcd019f925d5252db82c6135cb097b1da86110
Jessica Duell <duellrdh@gmail.com>
2019-01-19 15:26:53 -0500

    start

---
+++ helloWorld/src/helloWorld.cpp
@@ -0,0 +1,11 @@
+// Hello World a basic program
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	cout << "Hello World! Typing this out without a template." << endl;
+
+	return 0;
+}
