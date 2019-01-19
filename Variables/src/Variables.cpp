d955f1902380f75519fb10e21586a8c7c63b618f
Jessica Duell <duellrdh@gmail.com>
2019-01-19 16:49:10 -0500

    added string variables

--- Variables/src/Variables.cpp
+++ Variables/src/Variables.cpp
@@ -8,6 +8,9 @@ int main (){
 	int numberDogs = 5;
 	int numberAnimals = numberCats + numberDogs;
 
+	cout << "Starting int variables: " << endl;
+	cout << endl;
+
 	cout << "Hello" << endl;
 	cout << "Number of cats: " << numberCats << endl;
 	cout << "Number of dogs: " << numberDogs << endl;
@@ -16,6 +19,15 @@ int main (){
 	cout << "New dog acquired " << endl;
 	numberDogs = numberDogs + 1;
 	cout << "Number of dogs: " << numberDogs << endl;
+
+	cout << "Starting string variables: " << endl;
+	cout << endl;
+ 	string text1 = "Hello ";
+	string text2 = "world";
+	string text3 = text1 + text2;
+
+	cout << text1 << text2 << endl;
+	cout << text3<< endl;
 
 	return 0;
 }
