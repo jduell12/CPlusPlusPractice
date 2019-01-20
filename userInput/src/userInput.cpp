1faa26d95922f2a624c1e0bb8a597e24ed89b633
Jessica Duell <duellrdh@gmail.com>
2019-01-19 20:29:48 -0500

    completed user input

--- userInput/src/userInput.cpp
+++ userInput/src/userInput.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+int main (){
+
+	string input;
+	int value;
+
+	cout << "Enter your name: " << flush;
+	cin >> input;
+	cout << "You entered: " << input << endl;
+
+
+	cout << "Enter a number: " << flush;
+	cin >> value;
+	cout << "You entered: " << value << endl;
+
+	return 0;
+}
